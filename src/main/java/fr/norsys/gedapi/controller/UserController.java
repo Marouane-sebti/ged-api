c85de39bbc51f426b6d1c85214ded4a96e709abb
yaimadeddine <yo.aimadeddine@gmail.com>
2024-04-17 09:23:23 +0100

    user registre/findByUsername/findById

---
+++ src/main/java/fr/norsys/gedapi/controller/UserController.java
@@ -0,0 +1,28 @@
+package fr.norsys.gedapi.controller;
+
+import fr.norsys.gedapi.model.User;
+import fr.norsys.gedapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/registre")
+    public int registre(User user) {
+        return userService.registre(user);
+    }
+    @GetMapping("/findByUsername")
+    public User findByUsername(String username) {
+        return userService.findByUsername(username);
+    }
+    @GetMapping("/findById")
+    public User findById(int id) {
+        return userService.findById(id);
+    }
+}
